2387ce88d68d1635b94ca0d6a6f1bffec1d7138f
danji90 <danji_ma90@hotmail.com>
2018-07-30 17:26:45 +0200

    added FoI functions, not finished

---
+++ R/FoI.R
@@ -0,0 +1,65 @@
+#' @title Loads location data from any SensorThings API
+#' @description This function parses SensorTHings JSON data and stores it in an R data frame
+#' @param url A SensorThings API url (string!) containing the data in SensorThings web standard
+#' @return A data frame object containing data from url/Things
+#' @export
+#' @examples
+#'x = senseLocations("https://toronto-bike-snapshot.sensorup.com/v1.0")
+#'x
+#'
+#'v = senseLocations("https://tasking-test.sensorup.com/v1.0")
+#'v
+#'
+#'a = senseLocations("http://example.sensorup.com/v1.0")
+#'a
+
+senseFoI = function (url){
+  FoIExt = "FeaturesOfInterest"
+  FoIUrl = paste0(url, "/", FoIExt)
+  FoIJSON = jsonlite::fromJSON(FoIUrl)
+  FoIs = FoIJSON$value
+  return(FoIs)
+}
+
+#' @title Create a "thingLocation" data frame
+#' @description Creates a data frame from a previously parsed SensorThings JSON location object with the added class "thingLocation"
+#' @param FoIDF A data frame created using senseLocations (formatted according to SensorThings API)
+#' @return Data frame of class "thingLocation"
+#' @export
+#' @examples
+#'n = senseLocations("http://example.sensorup.com/v1.0")
+#'u = makeThingLocation(n)
+#'u
+#'
+
+makeThingFoI = function(FoIDF){
+
+  # Limit to point data
+  FoIDF = FoIDF[FoIDF$location$type=="Point", ]
+
+  # Stores the longitude and latitude values from SensorThings JSON objects into a table, then converts to data frame
+  coords = do.call(rbind, FoIDF$location$coordinates)
+  coords = data.frame(coords)
+
+  # Define data columns
+  id = FoIDF[1]
+  selfLink = FoIDF[2]
+  featureType = FoIDF$location$type
+  address =FoIDF[4]
+
+  # Create new object with data columns
+  locObj = data.frame(id, address, selfLink, featureType, coords$X1, coords$X2, stringsAsFactors = FALSE)
+
+  # Define data frame column header
+  colnames(locObj) = c("id", "address", "selfLink", "featureType", "long", "lat")
+
+  # Convert lat + long values to numeric
+  locObj$long = as.numeric(locObj$long)
+  locObj$lat = as.numeric(locObj$lat)
+
+  # Append class "thingLocation"
+  class(locObj) = append(class(locObj), "thingLocation")
+
+  return(locObj)
+}
+
